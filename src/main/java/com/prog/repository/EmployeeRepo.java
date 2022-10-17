b6b07eac951d3292dd93457dfe627a6261b28144
mayurpeopletech <113187146+mayurpeopletech@users.noreply.github.com>
2022-10-17 18:55:54 +0530

    Mayur Spring Project

---
+++ src/main/java/com/prog/repository/EmployeeRepo.java
@@ -0,0 +1,9 @@
+package com.prog.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.prog.entity.Employee;
+@Repository
+public interface EmployeeRepo  extends JpaRepository<Employee, Integer>
+{
+   
+}
---
+++ src/main/java/firstspring/springcode/SpringcodeApplication.java
@@ -0,0 +1,12 @@
+package firstspring.springcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcodeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcodeApplication.class, args);
+	}
+
+}
